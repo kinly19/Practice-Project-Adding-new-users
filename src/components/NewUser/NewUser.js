f5c495e056fc2f2edbf3247c001c349becb084be
unknown <kinchoily@hotmail.com>
2021-09-28 18:59:20 +0100

    NewUser.js created onSaveUserDataHandler function to be passed into UserForm component

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,10 +1,21 @@
 import UserForm from './UserForm';
 import './NewUser.scss';
 
-const NewUser = () => {
+const NewUser = ({onAddNewUser}) => {
+
+    const onSaveUserDataHandler = (usersData) => {
+        const newUserData = { //make a new object
+            ...usersData, //spread usersData coming from form
+            id:Math.random().toString() //add additional property
+        };
+
+        console.log(newUserData)// pass this up to app.js
+        onAddNewUser(newUserData);
+    }
+
   return (
     <div className="new-user">
-      <UserForm />
+      <UserForm onSaveUserData={onSaveUserDataHandler}/>
     </div>
   );
 };
