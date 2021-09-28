7d2df21dc6c92eb3db343511677f6e3c4a8c20db
unknown <kinchoily@hotmail.com>
2021-09-28 19:36:32 +0100

    App.js created an array of objects to store default data for state to use

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import NewUser from './components/NewUser/NewUser';
 
 
 function App() {
+
+  const initialData = [
+    {
+      id: Math.random().toString(),  
+      Name: "Crim",
+      Age: "23"
+    },
+    {
+      id: Math.floor(Math.random()),  
+      Name: "Qween",
+      Age: "25"
+    },
+  ];
+
+  const [user, setUser] = useState(initialData)
+
   return (
     <div className="App">
      <h1>Practice project</h1>
