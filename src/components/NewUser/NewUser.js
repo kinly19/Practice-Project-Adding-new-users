ca8f2ec54458b47f43688145f4e780604de20955
unknown <kinchoily@hotmail.com>
2021-09-28 16:08:49 +0100

    NewUser component and styling

---
+++ src/components/NewUser/NewUser.js
@@ -0,0 +1,12 @@
+import UserForm from './UserForm';
+import './NewUser.scss';
+
+const NewUser = () => {
+  return (
+    <div className="new-user">
+      <UserForm />
+    </div>
+  );
+};
+
+export default NewUser; 
