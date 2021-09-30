c223b77c627df9bcf6788a5994546a7d9953e00a
unknown <kinchoily@hotmail.com>
2021-09-30 16:42:18 +0100

    NewUser.js formatted code and removed unwanted console.log no changes made to actual logic of code

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -2,29 +2,31 @@ import UserForm from './UserForm';
 import './NewUser.scss';
 import Card from '../UI/Card';
 
-const NewUser = ({onAddNewUser, onIsInputValid, onIsAgeValid}) => {
-
-    const onSaveUserDataHandler = (usersData) => {
-        const newUserData = { //make a new object
-            ...usersData, //spread usersData coming from form
-            id:Math.random().toString() //add additional property
-        };
-
-        console.log(newUserData)// pass this up to app.js
-        onAddNewUser(newUserData);
-    }
-
-    const inputValidationHandler = (status) => { //pass down to Userform
-      onIsInputValid(status); //pass up to App.js
+const NewUser = ({ onAddNewUser, onIsInputValid, onIsAgeValid }) => {
+  
+  const onSaveUserDataHandler = (usersData) => {
+    const newUserData = {//make a new object
+      ...usersData, //spread usersData coming from form
+      id: Math.random().toString(), //add additional property
     };
+    onAddNewUser(newUserData); // pass this up to app.js as an argument
+  };
 
-    const ageValidationHandler = (ageStatus) => {
-      onIsAgeValid(ageStatus);
-    };
+  const inputValidationHandler = (status) => {//pass down to Userform
+    onIsInputValid(status); //pass up to App.js
+  };
+
+  const ageValidationHandler = (ageStatus) => {
+    onIsAgeValid(ageStatus);
+  };
 
   return (
     <Card className="new-user">
-      <UserForm onSaveUserData={onSaveUserDataHandler} onValidation={inputValidationHandler} onAgeValidation={ageValidationHandler}/>
+      <UserForm
+        onSaveUserData={onSaveUserDataHandler}
+        onValidation={inputValidationHandler}
+        onAgeValidation={ageValidationHandler}
+      />
     </Card>
   );
 };
